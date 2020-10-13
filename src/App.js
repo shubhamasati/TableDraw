c44f19c8c3abe185133cc7f528a58d06bcb21a48
Shubham Asati <shbham.asti128@gmail.com>
2020-10-13 17:59:20 +0530

    fixed warnings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import React, {useLayoutEffect, useState } from 'react';
-import rough from "roughjs/bundled/rough.esm";
+import React, { useLayoutEffect, useState } from 'react';
 
-function  saveToPng() {
-  const data = document.getElementById("canvas").toDataURL('image/png');
-  const anchor = document.createElement('a');
-  anchor.href = data;
-  anchor.download = 'image.png';
-  anchor.click();
-};
+
+
 
 function App() {
 
@@ -16,21 +10,21 @@ function App() {
   const [elementType, setElementType] = useState("line");
   const [background, setBackground] = useState(null);
   const [elementColor, setColor] = useState('#000');
-  
+
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d")
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
 
     // Checking if background is selected
     if (background != null) {
       ctx.drawImage(background, 100, 100, background.width, background.height);
     }
-   
+
     // rendering each element on canvas
-    elements.forEach( ({x1, y1, x2, y2, elementType, elementColor}) => {
-      
+    elements.forEach(({ x1, y1, x2, y2, elementType, elementColor }) => {
+
       if (elementType === "line") {
         console.log(elementColor)
         ctx.beginPath();
@@ -40,13 +34,13 @@ function App() {
         if ((x2 - x1) > (y2 - y1)) {
           ctx.lineTo(x1, y1);
           ctx.lineTo(x2, y1);
-        } else{
+        } else {
           ctx.lineTo(x1, y1);
           ctx.lineTo(x1, y2);
         }
-        
+
         ctx.stroke();
-      } 
+      }
       else {
         ctx.beginPath();
         ctx.strokeStyle = elementColor;
@@ -67,7 +61,7 @@ function App() {
     const y1 = clientY;
     const x2 = x1;
     const y2 = y1;
-    const updatedElement = {x1, y1, x2, y2, elementType, elementColor};
+    const updatedElement = { x1, y1, x2, y2, elementType, elementColor };
     setElements(prevState => [...prevState, updatedElement]);
 
   };
@@ -81,7 +75,7 @@ function App() {
     const { x1, y1 } = elements[index];
     const x2 = clientX;
     const y2 = clientY;
-    const updatedElement = {x1, y1, x2, y2, elementType, elementColor};
+    const updatedElement = { x1, y1, x2, y2, elementType, elementColor };
 
     const elementsCopy = [...elements];
     elementsCopy[index] = updatedElement;
@@ -94,7 +88,7 @@ function App() {
     setDrawing(false);
   };
 
-  function loadImageTocanvas(url) {
+  const loadImageTocanvas = (url) => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d")
     var img = new Image();
@@ -108,29 +102,34 @@ function App() {
     img.src = url;
   }
 
-  function changeColor(event) {
+  const changeColor = (event) => {
     setColor(event.target.value);
-    
+
   }
 
-  function loadCanvasWithInputFile(event) {
-    
+  const loadCanvasWithInputFile = (event) => {
     var files = event.target.files; // FileList object
     if (files.length === 0) return;
     var file = files[0];
 
-    console.log(file)
-    
     if (!file.type.match('image.*')) {
       alert("Not Image file")
       return;
     }
-
     const window_URL = window.URL || window.webkitURL;
-
     var imageURL = window_URL.createObjectURL(file);
     loadImageTocanvas(imageURL);
-}
+  }
+
+
+  const saveToPng = () => {
+    const data = document.getElementById("canvas").toDataURL('image/png');
+    const anchor = document.createElement('a');
+    anchor.href = data;
+    anchor.download = 'image.png';
+    anchor.click();
+  };
+
 
   return (
     <div className="App">
